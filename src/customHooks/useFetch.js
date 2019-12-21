eca5926175e2dd4ac9cd8a208367a14029c017da
Haekal Budiman <hbudiman45@gmail.com>
2019-12-21 13:39:57 +0700

    fix

--- src/customHooks/useFetch.js
+++ src/customHooks/useFetch.js
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react";
 const useFetch = url => {
   const [state, setState] = useState({ data: null, loading: false });
   useEffect(() => {
-    setState(state => ({ data: state.data, loading: true }));
-    fetch(url)
-      .then(x => x.text())
-      .then(y => setState({ data: y, loading: false }))
-      .catch(err => console.log(err));
+    async function f() {
+      setState(state => ({ data: state.data, loading: true }));
+      fetch(url)
+        .then(x => x.text())
+        .then(y => setState({ data: y, loading: false }))
+        .catch(err => console.log(err));
+    }
+    f();
   }, [url]);
   return state;
 };
